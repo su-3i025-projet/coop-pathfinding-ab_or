a0b3ff77503a296693deb012a1b7f8c44923210e
flavienaubard <36225611+flavienaubard@users.noreply.github.com>
2019-03-21 15:42:33 +0100

    Add files via upload

--- pySpriteWorld-forStudents/DiscreteWorld-coopPathFinding.py
+++ pySpriteWorld-forStudents/DiscreteWorld-coopPathFinding.py
@@ -32,11 +32,130 @@ def init(_boardname=None):
     game = Game('Cartes/' + name + '.json', SpriteBuilder)
     game.O = Ontology(True, 'SpriteSheet-32x32/tiny_spritesheet_ontology.csv')
     game.populate_sprite_names(game.O)
-    game.fps = 5  # frames per second
+    game.fps = 15 # frames per second
     game.mainiteration()
     game.mask.allow_overlaping_players = True
     #player = game.player
     
+    
+def astar(initState, goalState, wallStates):
+    
+    positionDepart = { "pos" : initState[0], "score": 0}
+    
+    coupexplore = [[positionDepart.get("pos"), 0, abs(initState[0][0] - goalState[0][0]) + abs(initState[0][1] - goalState[0][1]), None]]
+    reserve = []
+    while(True):
+        for i in [(0,1),(0,-1),(1,0),(-1,0)]: #on prend les nouveau coup      
+            newlig = positionDepart.get("pos")[0]+i[0]
+            newcol = positionDepart.get("pos")[1]+i[1]
+            newPos = (newlig,newcol)
+            if (newPos not in wallStates) and (newPos not in [coupexplore[i][0] for i in range(len(coupexplore))]) and newlig>=0 and newlig<20 and newcol>=0 and newcol<20 : 
+#tout ce if permet de faire ce que notre couppossible d'avant
+              
+              
+              #si on arrive a la pose final
+              if newPos in goalState:
+                listeCoups = []
+                listeCoups.append(newPos)   #on fait une nouvelle liste avec les nouveau coup qui respectent les critere
+
+                
+                for c in coupexplore:#{
+                    if c[0] == positionDepart.get("pos"):
+                        a = c
+                        break #}
+                                   
+                while a[3]:   
+                  #{
+                    listeCoups.append(a[0])
+                    newPos = a[3] 
+                    
+                    for c in coupexplore:
+                        if c[0] == newPos:
+                            a = c
+                            break 
+                  #}
+                  
+                  
+                  
+                #return de la fonction est la 
+                listeCoups.append(initState[0])
+                ltmp = []
+                for k in range(len(listeCoups),0,-1):
+                    ltmp.append(listeCoups[k-1])
+                return ltmp
+              if not (newlig,newcol) in reserve:#{
+                    score = positionDepart.get("score") + abs(newlig - goalState[0][0]) + abs(newcol - goalState[0][1])
+                    tmp = [newPos,positionDepart.get("score")+1,score,positionDepart.get("pos")]
+                    coupexplore.append(tmp)
+                    reserve.append(tmp)
+                    #}
+                          
+            
+                    
+        #boucle dans la reserve et prise de choix
+        minR = 100000000
+        choix = None
+        if reserve==[]:
+          return []
+        for i in reserve:
+            if i[2] < minR:
+                minR = i[2]
+                positionDepart = { "pos" : i[0], "score" : i[1] }
+                choix = i
+        reserve.remove(choix)
+        #on oublie pas d'enlever le choix pour pas faire plusieurs fois le meme choix
+    
+def collisionOp(init,goal,wallStates, i,coup,boolean,j,adv,chemin):
+  wS= wallStates.copy()
+  if boolean:
+    print("Collision réelle")
+    wS.append(coup)
+    
+  #if j<adv:  
+  ast=astar(init,goal,wS)
+  res=[]
+  print(i)
+  for j in range(i):
+    res.append((0,0))
+  for k in ast:
+    res.append(k)
+#  else:
+#    res=[]
+#    for l in range(len(chemin)):
+#      if l==i-1:
+#        res.append(chemin[l])
+#        res.append(chemin[l])
+#      else:
+#        res.append(chemin[l])
+  return res
+
+def collisionCoop(init_States,goal_States, wallStates):
+  res=[]
+  wS=wallStates.copy()
+  for i in range(0,len(init_States)):
+    res.append([])
+    res[i]=astar([init_States[i]],[goal_States[i]],wS)
+    if res[i]==[]:
+      print("chemin deja pris")
+      res[i]=astar([init_States[i]],[goal_States[i]],wallStates)
+    for j in res[i]:
+      wS.append(j)
+  return res
+      
+
+def voisinrandom(posplayer,wallStates):
+  res=[]
+  if (posplayer[0],posplayer[1]-1) not in wallStates:
+    res.append((posplayer[0],posplayer[1]-1))
+  if (posplayer[0],posplayer[1]+1) not in wallStates:
+    res.append((posplayer[0],posplayer[1]+1))
+  if (posplayer[0]-1,posplayer[1]) not in wallStates:
+    res.append((posplayer[0]-1,posplayer[1]))
+  if (posplayer[0]+1,posplayer[1]) not in wallStates:
+    res.append((posplayer[0]+1,posplayer[1]))
+  return random.choice(res)
+  
+  
 def main():
 
     #for arg in sys.argv:
@@ -69,6 +188,7 @@ def main():
     # on localise tous les objets ramassables
     goalStates = [o.get_rowcol() for o in game.layers['ramassable']]
     print ("Goal states:", goalStates)
+                
         
     # on localise tous les murs
     wallStates = [w.get_rowcol() for w in game.layers['obstacle']]
@@ -76,7 +196,7 @@ def main():
     
     #-------------------------------
     # Placement aleatoire des fioles 
-    #-------------------------------
+    #---------------------------print("old",listeAstar[j])----
     
     
     # on donne a chaque joueur une fiole a ramasser
@@ -89,53 +209,89 @@ def main():
     
         
     # bon ici on fait juste plusieurs random walker pour exemple...
-    
-    posPlayers = initStates
-
-    for i in range(iterations):
+    while(True):
+      posPlayers = initStates
+      listeAstar = []
+      listecollision=[]
+      estarrive=[]
+      collisiones=[]
+      wallS = wallStates.copy()
+      for i in range(nbPlayers):
         
-        for j in range(nbPlayers): # on fait bouger chaque joueur séquentiellement
-            row,col = posPlayers[j]
-
-            x_inc,y_inc = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
-            next_row = row+x_inc
-            next_col = col+y_inc
-            # and ((next_row,next_col) not in posPlayers)
-            if ((next_row,next_col) not in wallStates) and next_row>=0 and next_row<=19 and next_col>=0 and next_col<=19:
-                players[j].set_rowcol(next_row,next_col)
-                print ("pos :", j, next_row,next_col)
-                game.mainiteration()
-    
-                col=next_col
-                row=next_row
-                posPlayers[j]=(row,col)
-            
+        #Pour coop oportuniste
+        #listeAstar.append(astar([players[i].get_rowcol()],[goalStates[i]],wallS))
+        
+        listecollision.append(False)
+        estarrive.append(False)
+        collisiones.append(None)
+        
+      #pour coopérative de base
+      listeAstar=collisionCoop(initStates,goalStates,wallS)
       
-        
-            
-            # si on a  trouvé un objet on le ramasse
-            if (row,col) in goalStates:
+      #maxLen = longueur du plus long parcours
+      #iteration sur maxlen avec un while (ne pas oublier d'iterer i)
+      for i in range(iterations):
+          for j in range(nbPlayers): # on fait bouger chaque joueur séquentiellement
+              if i >= len(listeAstar[j]): # ce if sert a gerer le cas ou un ou deux
+                #personne ont trouvé leur fiole pour permettre d'attendre que tout le monde 
+                #ai la sienne
+                continue
+              next_row,next_col=listeAstar[j][i]
+              #collision
+              if (next_row, next_col) in posPlayers:
+                if (next_row, next_col)==posPlayers[j]:
+                  print("Début de la recherche joueur ",j)
+                else:
+                  listecollision[j]=True
+                  for k in range(len(posPlayers)):
+                    if posPlayers[k]==(next_row, next_col):
+                      print('Collsion entre ',j, 'et', k)
+                      adv=k
+                  #if collisiones[j]==None:
+                  listeAstar[j]=collisionOp([posPlayers[j]],[goalStates[j]],wallS,i,(next_row,next_col),True,j,adv,listeAstar[j])
+                     #collisiones[j]=adv
+                  #elif collisiones[j]==adv:
+                    #collisiones[j]=None
+              #else:
+               # if listecollision[j]==True:
+               #   listeAstar[j]=collision([players[j].get_rowcol()],[goalStates[j]],wallStates,i,(next_row,next_col),False)
+                #  listecollision[j]==False
+              if len(listeAstar[j])==i:
+                if players[j] in goalStates:
+                  print("VOISIN SUR FIOLE")
+                  next_row,next_col=voisinrandom(players[j],wallS)
+                else:
+                  continue
+              else:
+                next_row,next_col=listeAstar[j][i]
+              players[j].set_rowcol(next_row,next_col)
+              #print ("pos :", j, next_row,next_col)
+              game.mainiteration()
+  
+              col=next_col
+              row=next_row
+              posPlayers[j]=(row,col)
+              # si on a  trouvé un objet on le ramasse
+              if (row,col) == goalStates[j]:
+                #if (row,col) == goalStates[j]:
                 o = players[j].ramasse(game.layers)
                 game.mainiteration()
+                wallS.append((row,col))
                 print ("Objet trouvé par le joueur ", j)
+                print(goalStates)
                 goalStates.remove((row,col)) # on enlève ce goalState de la liste
                 score[j]+=1
-                
-        
                 # et on remet un même objet à un autre endroit
-                x = random.randint(1,19)
-                y = random.randint(1,19)
-                while (x,y) in wallStates:
-                    x = random.randint(1,19)
-                    y = random.randint(1,19)
+                x = random.randint(1,5)
+                y = random.randint(1,5)
+                while (x,y) in wallStates or (x,y) in goalStates or (x,y) in posPlayers:
+                  x = random.randint(1,5)
+                  y = random.randint(1,5)
                 o.set_rowcol(x,y)
-                goalStates.append((x,y)) # on ajoute ce nouveau goalState
+                goalStates.insert(j,(x,y))
+                #goalStates.append((x,y)) # on ajoute ce nouveau goalState
                 game.layers['ramassable'].add(o)
                 game.mainiteration()                
-                
-                break
-            
-    
     print ("scores:", score)
     pygame.quit()
     
@@ -148,3 +304,7 @@ if __name__ == '__main__':
     
 
 
+
+  
+  
+  
--- pySpriteWorld-forStudents/DiscreteWorldAStar-playerVersion.py
+++ pySpriteWorld-forStudents/DiscreteWorldAStar-playerVersion.py
@@ -40,6 +40,76 @@ def init(_boardname=None):
     game.mainiteration()
     player = game.player
     
+def astar(initState, goalState, wallStates):
+    
+    positionDepart = { "pos" : initState[0], "score": 0}
+    
+    coupexplore = [[positionDepart.get("pos"), 0, abs(initState[0][0] - goalState[0][0]) + abs(initState[0][1] - goalState[0][1]), None]]
+    reserve = []
+    while(True):
+        for i in [(0,1),(0,-1),(1,0),(-1,0)]: #on prend les nouveau coup      
+            newlig = positionDepart.get("pos")[0]+i[0]
+            newcol = positionDepart.get("pos")[1]+i[1]
+            newPos = (newlig,newcol)
+            if (newPos not in wallStates) and (newPos not in [coupexplore[i][0] for i in range(len(coupexplore))]) and newlig>=0 and newlig<20 and newcol>=0 and newcol<20 : 
+#tout ce if permet de faire ce que notre couppossible d'avant
+              
+              
+              #si on arrive a la pose final
+              if newPos in goalState:
+                listeCoups = []
+                listeCoups.append(newPos)   #on fait une nouvelle liste avec les nouveau coup qui respectent les critere
+                
+                
+                for c in coupexplore:#{
+                    if c[0] == positionDepart.get("pos"):
+                        a = c
+                        break #}
+                #
+                        
+                while a[3]!= None:   #a[3] c'est les peres  
+                  #{
+                    listeCoups.append(a[0])
+                    newPos = a[3] 
+                    
+                    for c in coupexplore:
+                        if c[0] == newPos:
+                            a = c
+                            break 
+                  #}
+                  
+                  
+                  
+                #return de la fonction est la 
+                listeCoups.append(initState[0])
+                ltmp = []
+                for k in range(len(listeCoups),0,-1):
+                    ltmp.append(listeCoups[k-1])
+                return ltmp
+              if not (newlig,newcol) in reserve:#{
+                    score = positionDepart.get("score") + abs(newlig - goalState[0][0]) + abs(newcol - goalState[0][1])
+                    tmp = [newPos,positionDepart.get("score")+1,score,positionDepart.get("pos")]
+                    coupexplore.append(tmp)
+                    reserve.append(tmp)
+                    #}
+            
+            
+                    
+        #boucle dans la reserve et prise de choix
+        minR = 100000000
+        choix = None
+        for i in reserve:
+            if i[2] < minR:
+                minR = i[2]
+                positionDepart = { "pos" : i[0], "score" : i[1] }
+                choix = i
+        reserve.remove(choix)
+        #on oublie pas d'enlever le choix pour pas faire plusieurs fois le meme choix
+    
+    
+    
+    
+      
 def main():
 
     #for arg in sys.argv:
@@ -73,11 +143,7 @@ def main():
     
     #-------------------------------
     # Building the best path with A*
-    #-------------------------------
-    
-
-    
-    
+    #-------------------------------    
         
     #-------------------------------
     # Moving along the path
@@ -89,23 +155,14 @@ def main():
     row,col = initStates[0]
     #row2,col2 = (5,5)
 
-    for i in range(iterations):
-    
-    
-        x_inc,y_inc = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
-        next_row = row+x_inc
-        next_col = col+y_inc
-        if ((next_row,next_col) not in wallStates) and next_row>=0 and next_row<=20 and next_col>=0 and next_col<=20:
-            player.set_rowcol(next_row,next_col)
-            print ("pos 1:",next_row,next_col)
-            game.mainiteration()
+    path = astar(initStates, goalStates, wallStates)
+    for i in range(len(path)):
+      
+        next_row,next_col = path[i]       
+        player.set_rowcol(next_row,next_col)
+        print ("pos 1:",next_row,next_col)
+        game.mainiteration()
 
-            col=next_col
-            row=next_row
-
-            
-        
-            
         # si on a  trouvé l'objet on le ramasse
         if (row,col)==goalStates[0]:
             o = game.player.ramasse(game.layers)
